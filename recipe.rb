eb61c97245ce89d64850c9d9a0ef1d4e5a660bb6
Yukich <june.ymd@gmail.com>
2015-07-04 11:17:03 +0900

    spec5: first commit

--- recipe.rb
+++ recipe.rb
@@ -2,6 +2,15 @@
 
 recipe_file = File.open("recipe_data.txt", "r")
 
+recipe_array = Array.new
+
+
 while dish_name = recipe_file.gets
-  print dish_name
+  recipe_array.push(dish_name.chomp)
+end
+
+
+recipe_array.each_with_index do |recipe_name, id|
+  string_to_print = id.to_s + ": " + recipe_name
+  print string_to_print + "\n"
 end
