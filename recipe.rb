6226d78b5d1af32e36f614080061cf9046398db8
Yukich <june.ymd@gmail.com>
2015-07-04 11:31:00 +0900

    spec6: first commit

--- recipe.rb
+++ recipe.rb
@@ -1,16 +1,27 @@
 #!/usr/bin/ruby
 
-recipe_file = File.open("recipe_data.txt", "r")
 
+recipe_file = File.open("recipe_data.txt", "r")
 recipe_array = Array.new
 
-
+# レシピファイルを読み込み
 while dish_name = recipe_file.gets
   recipe_array.push(dish_name.chomp)
 end
 
 
-recipe_array.each_with_index do |recipe_name, id|
-  string_to_print = id.to_s + ": " + recipe_name
-  print string_to_print + "\n"
+# ユーザの入力
+print "IDを入力してエンターを押してください. IDを指定しない場合はそのままエンターを押してください.\n"
+user_input_string = gets.chomp
+
+
+# 入力が無かった場合
+if user_input_string == ""
+  recipe_array.each_with_index do |recipe_name, id|
+    string_to_print = id.to_s + ": " + recipe_name
+    print string_to_print + "\n"
+  end
 end
+
+# 入力があった場合
+print recipe_array[user_input_string.to_i] + "\n"
